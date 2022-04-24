4b2a9ad69ce779d78d29491e978d8e46b98b1581
psalire <philip.salire@sjsu.edu>
2022-04-24 01:25:06 -0700

    update plotting

--- DataPlotter.py
+++ DataPlotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import logging
 
 logging.basicConfig(
@@ -11,29 +12,71 @@ class DataPlotter:
 
     def __init__(self, data, db):
         self.data = data
-        self.db = db
+        self.db = db.capitalize()
 
-    def plot_means(self, title, means, lang, db):
+    def plot_means(self, title, means, lang):
+        fname = f'{self.db}_{lang}_means.png'
         try:
             fig, ax = plt.subplots()
             ax.bar(title, means)
 
-            ax.set_title(f'{lang.capitalize()} Benchmark',
+            ax.set_title(f'{lang.capitalize()} {self.db} Benchmark',
                     loc ='center',)
             ax.set_xticklabels(title,rotation=90)
 
             plt.xlabel('Operation', fontweight ='bold')
             plt.ylabel('Mean Time(ms)', fontweight ='bold')
-            fname = f'{lang}_{db}_MEANS.png'
             plt.savefig(fname, bbox_inches='tight')
             logging.info(f'Created plot {fname}')
             # plt.show()
         except Exception as e:
-            logging.error('Create plot PNG failed.')
+            logging.error(f'Create plot {fname} failed.')
             logging.exception(e)
 
+    def plot_language_comparison(self):
+        fname = f'{self.db}_language_comparison.png'
+        try:
+            d = {}
+            for lang in self.data:
+                d[lang] = {}
+                for b in self.data[lang]:
+                    if self.data[lang][b]['success'] is False:
+                        continue
+                    d[lang][b] = self.data[lang][b]['time']['mean']
+            df = pd.DataFrame(d)
+
+            ax = df.plot.bar()
+            ax.set_xlabel('Benchmark')
+            ax.set_ylabel('Time (ms)')
+            ax.set_title(f'{self.db} Benchmarks')
+            plt.savefig(fname, bbox_inches='tight')
+            logging.info(f'Created plot {fname}')
+        except Exception as e:
+            logging.error(f'Create plot {fname} failed.')
+            logging.exception(e)
+
+    def plot_mean_max_min(self):
+        for lang in self.data:
+            d = {'mean': {}, 'max': {}, 'min': {}}
+            for b in self.data[lang]:
+                if self.data[lang][b]['success'] is False:
+                    continue
+                for k in d.keys():
+                    d[k][b] = self.data[lang][b]['time'][k]
+            df = pd.DataFrame(d)
+
+            fname = f'{self.db}_{lang}_mean_max_min.png'
+            ax = df.plot.bar()
+            ax.set_xlabel('Benchmark')
+            ax.set_ylabel('Time (ms)')
+            ax.set_title(f'{self.db} Benchmarks')
+            plt.savefig(fname, bbox_inches='tight')
+            logging.info(f'Created plot {fname}')
 
     def result_display(self):
+        self.plot_language_comparison()
+        self.plot_mean_max_min()
+
         title_op = []
         mean_op = []
 
@@ -48,7 +91,7 @@ class DataPlotter:
                 mean_op.append(benchmark_data['time']['mean'])
 
             if len(title_op) > 0:
-                self.plot_means(title_op, mean_op, language, self.db)
+                self.plot_means(title_op, mean_op, language)
 
             title_op.clear()
             mean_op.clear()
